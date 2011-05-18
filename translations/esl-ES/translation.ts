61f66f102632878407267864271ae3ee47f90d69
Jérôme Vieilledent <lolautruche@gmail.com>
2011-05-18 14:22:05 +0000

    Added spanish translation (thanks Alex Muñoz)

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,289 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES" sourcelanguage="en_GB">
+<context>
+    <name>extension/sqliimport</name>
+    <message>
+        <source>Request a new immediate import</source>
+        <translation>Solicitar una nueva importación inmediata</translation>
+    </message>
+    <message>
+        <source>Import handler</source>
+        <translation>Controlador de la importación</translation>
+    </message>
+    <message>
+        <source>Options (facultative)</source>
+        <translation>Opciones (facultativo)</translation>
+    </message>
+    <message>
+        <source>One option per line : optionName=optionValue</source>
+        <translation>Una opción por línea: = valorDeOpción</translation>
+    </message>
+    <message>
+        <source>Schedule an import</source>
+        <translation>Programa una importación</translation>
+    </message>
+    <message>
+        <source>Import label</source>
+        <translation>Etiqueta de importación</translation>
+    </message>
+    <message>
+        <source>Choose a start date (YYYY-mm-dd)</source>
+        <translation>Elige fecha de inicio (AAAA-mm-dd)</translation>
+    </message>
+    <message>
+        <source>Hour</source>
+        <translation>Hora</translation>
+    </message>
+    <message>
+        <source>Minutes</source>
+        <translation>Minutos</translation>
+    </message>
+    <message>
+        <source>Back to import list</source>
+        <translation>Volver a la lista de importación</translation>
+    </message>
+    <message>
+        <source>Import list</source>
+        <translation>Lista de importación</translation>
+    </message>
+    <message>
+        <source>No imports</source>
+        <translation>No hay importaciones</translation>
+    </message>
+    <message>
+        <source>Import type</source>
+        <translation>Tipo de importación</translation>
+    </message>
+    <message>
+        <source>Params</source>
+        <translation>Parámetros</translation>
+    </message>
+    <message>
+        <source>User</source>
+        <translation>Usuario</translation>
+    </message>
+    <message>
+        <source>Requested on</source>
+        <translation>Pedido en</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation>Progreso</translation>
+    </message>
+    <message>
+        <source>Progression notes</source>
+        <translation>Notas del progreso</translation>
+    </message>
+    <message>
+        <source>Duration</source>
+        <translation>Duración</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to cancel this import ?</source>
+        <translation>¿Está seguro que desea cancelar esta importación?</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to interrupt this import ?</source>
+        <translation>¿Está seguro que desea interrumpir esta importación?</translation>
+    </message>
+    <message>
+        <source>Interrupt</source>
+        <translation>Interrupción</translation>
+    </message>
+    <message>
+        <source>Import management</source>
+        <translation>Gestor de importación</translation>
+    </message>
+    <message>
+        <source>Scheduled import(s)</source>
+        <translation>Importaciones programadas</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to purge import history ?</source>
+        <translation>¿Está seguro que desea purgar el historial de importación?</translation>
+    </message>
+    <message>
+        <source>Purge import history</source>
+        <translation>Purgar el historial de importación</translation>
+    </message>
+    <message>
+        <source>Scheduled import list</source>
+        <translation>Lista de importaciones programadas</translation>
+    </message>
+    <message>
+        <source>Add a scheduled import</source>
+        <translation>Añadir una importación programada</translation>
+    </message>
+    <message>
+        <source>No scheduled imports</source>
+        <translation>No hay importaciones programadas</translation>
+    </message>
+    <message>
+        <source>Label</source>
+        <translation>Etiqueta</translation>
+    </message>
+    <message>
+        <source>Next import</source>
+        <translation>Siguiente importación</translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation>Activa</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Edit import</source>
+        <translation>Editar importación</translation>
+    </message>
+    <message>
+        <source>Remove scheduled import</source>
+        <translation>Eliminar importación programada</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to remove this scheduled import ?</source>
+        <translation>¿Está seguro que desea eliminar esta importación programada?</translation>
+    </message>
+    <message>
+        <source>Are you sure you want purge to import history ?</source>
+        <translation>¿Está seguro que desea purgar el historial de importación?</translation>
+    </message>
+    <message>
+        <source>Edit a scheduled import</source>
+        <translation>Editar una importación prevista</translation>
+    </message>
+    <message>
+        <!-- Lang file for JSCalendar -->
+        <source>javascript/lang/en.js</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Activate import</source>
+        <translation>Activar importación</translation>
+    </message>
+    <message>
+        <source>Import management list</source>
+        <translation>Lista de importaciones </translation>
+    </message>
+</context>
+<context>
+    <name>extension/sqliimport/schedulefrequency</name>
+    <message>
+        <source>Frequency</source>
+        <translation>Frecuencia</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ninguna</translation>
+    </message>
+    <message>
+        <source>Hourly</source>
+        <translation>Todas las horas</translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation>Todos los días</translation>
+    </message>
+    <message>
+        <source>Weekly</source>
+        <translation>Todas las semanas</translation>
+    </message>
+    <message>
+        <source>Monthly</source>
+        <translation>Todos los meses</translation>
+    </message>
+    <message>
+        <source>Every</source>
+        <translation>Cada</translation>
+    </message>
+    <message>
+        <source>minutes (not less than 5min)</source>
+        <translation>minutos (no menos de 5 minutos)</translation>
+    </message>
+</context>
+<context>
+    <name>extension/sqliimport/error</name>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>SQLIPendingImport : Unknown attribute &quot;%attribute&quot;</source>
+        <translation>SQLIPendingImport : Atributo desconocido &quot;%attribute&quot;</translation>
+    </message>
+    <message>
+        <source>Please choose a correct date</source>
+        <translation>Por favor, elija una fecha correcta</translation>
+    </message>
+    <message>
+        <source>No import item found with ID #%importID</source>
+        <translation>No existe ningún item con ID #%importID</translation>
+    </message>
+    <message>
+        <source>Unknown alter import action &apos;%action&apos;</source>
+        <translation>Action de alteración desconocida &apos;%action&apos;</translation>
+    </message>
+    <message>
+        <source>Please choose a frequency greater than 5min</source>
+        <translation>Por favor, elija una frecuencia superior a 5 minutos</translation>
+    </message>
+</context>
+<context>
+    <name>extension/sqliimport/type</name>
+    <message>
+        <source>Immediate</source>
+        <translation>Inmediato</translation>
+    </message>
+    <message>
+        <source>Scheduled</source>
+        <translation>Programada</translation>
+    </message>
+    <message>
+        <source>Pending</source>
+        <translation>Pendiente</translation>
+    </message>
+    <message>
+        <source>Running</source>
+        <translation>En curso</translation>
+    </message>
+    <message>
+        <source>Failed</source>
+        <translation>Fallida</translation>
+    </message>
+    <message>
+        <source>Completed</source>
+        <translation>Completada</translation>
+    </message>
+    <message>
+        <source>Canceled</source>
+        <translation>Cancelada</translation>
+    </message>
+    <message>
+        <source>Interrupted</source>
+        <translation>Interrumpida</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/navigationpart</name>
+    <message>
+        <source>Import management</source>
+        <comment>Navigation part</comment>
+        <translation>Gestión de importaciones</translation>
+    </message>
+</context>
+</TS>
